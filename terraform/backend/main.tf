7828f4da3f14312a7387bce15878ebe336ce66d4
saiteja <saiteja.koppala@nttadata.com>
2025-07-17 19:50:30 +0530

    Add VPC, subnets, route tables, and security group for ECS deployment

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -15,6 +15,106 @@ provider "aws" {
   region     = "eu-central-1"
 }
 
+# --- New VPC and networking resources ---
+resource "aws_vpc" "be_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "quarkus-app-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "be_igw" {
+  vpc_id = aws_vpc.be_vpc.id
+  tags = {
+    Name = "quarkus-app-igw"
+  }
+}
+
+resource "aws_subnet" "be_subnet_a" {
+  vpc_id                  = aws_vpc.be_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "quarkus-app-subnet-a"
+  }
+}
+
+resource "aws_subnet" "be_subnet_b" {
+  vpc_id                  = aws_vpc.be_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "quarkus-app-subnet-b"
+  }
+}
+
+resource "aws_subnet" "be_subnet_c" {
+  vpc_id                  = aws_vpc.be_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-central-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "quarkus-app-subnet-c"
+  }
+}
+
+resource "aws_route_table" "be_rt" {
+  vpc_id = aws_vpc.be_vpc.id
+  tags = {
+    Name = "quarkus-app-rt"
+  }
+}
+
+resource "aws_route" "be_route" {
+  route_table_id         = aws_route_table.be_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.be_igw.id
+}
+
+resource "aws_route_table_association" "be_rta_a" {
+  subnet_id      = aws_subnet.be_subnet_a.id
+  route_table_id = aws_route_table.be_rt.id
+}
+
+resource "aws_route_table_association" "be_rta_b" {
+  subnet_id      = aws_subnet.be_subnet_b.id
+  route_table_id = aws_route_table.be_rt.id
+}
+
+resource "aws_route_table_association" "be_rta_c" {
+  subnet_id      = aws_subnet.be_subnet_c.id
+  route_table_id = aws_route_table.be_rt.id
+}
+
+# --- Security group allowing traffic from all to port 8080 ---
+resource "aws_security_group" "be_sg" {
+  name        = "quarkus-app-sg"
+  description = "Allow all inbound traffic to port 8080"
+  vpc_id      = aws_vpc.be_vpc.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "quarkus-app-sg"
+  }
+}
+
 # BE Quarkus App configuration start
 # ECS Task Definition for Quarkus App (Fargate)
 resource "aws_ecs_task_definition" "be_app_task_def" {
@@ -73,8 +173,8 @@ resource "aws_ecs_service" "be_app_service" {
   depends_on = [aws_lb_listener.be_listener]
 
   network_configuration {
-    subnets          = ["subnet-0cb003bdf4847a5da", "subnet-0df54116201e545d0", "subnet-02bc517d0f103bde0"]
-    security_groups  = ["sg-009cc415372b1beee"]
+    subnets          = [aws_subnet.be_subnet_a.id, aws_subnet.be_subnet_b.id, aws_subnet.be_subnet_c.id]
+    security_groups  = [aws_security_group.be_sg.id]
     assign_public_ip = true
   }
 }
@@ -84,8 +184,8 @@ resource "aws_lb" "be_alb" {
   name               = "be-app-service"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["sg-009cc415372b1beee"]
-  subnets            = ["subnet-0cb003bdf4847a5da", "subnet-0df54116201e545d0", "subnet-02bc517d0f103bde0"]
+  security_groups    = [aws_security_group.be_sg.id]
+  subnets            = [aws_subnet.be_subnet_a.id, aws_subnet.be_subnet_b.id, aws_subnet.be_subnet_c.id]
 }
 
 # Target group for BE service
@@ -93,7 +193,7 @@ resource "aws_lb_target_group" "be_tg" {
   name        = "be-tg"
   port        = 8080
   protocol    = "HTTP"
-  vpc_id      = "vpc-079bb37d3813c4b6a"
+  vpc_id      = aws_vpc.be_vpc.id
   target_type = "ip"
   health_check {
     path                = "/home"
